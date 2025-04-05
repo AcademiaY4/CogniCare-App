63340b14073d5ca5f81aa17448db09a964d8dca2
Mohammad Shabeer <moshdev2213@gmail.com>
2025-04-05 11:39:17 +0530

    feat:CIAddedForGateway

--- backend/gateway-service/src/app.js
+++ backend/gateway-service/src/app.js
@@ -5,9 +5,6 @@ import corsOption from "./Config/CorsConfig.js";
 import proxy from 'express-http-proxy'
 import response from "./Utils/ResponseHandler/ResponseHandler.js";
 import ResTypes from "./Utils/Constants/ResTypes.js";
-import AuthHeaderYup from "./Utils/Validation/AuthHeaderYup.js";
-import HeaderValidation from "./MiddleWare/YupSchema/HeaderValidation.js"
-import Tokenware from "./MiddleWare/Tokenware/Tokenware.js";
 import { EnvConfig } from "./Config/EnvConfigs.js";
 
 const app = express()
@@ -20,16 +17,16 @@ app.get('/', (req, res) => {
 })
 
 // proxies
-app.use('/api/v1/auth', proxy(EnvConfig.AUTH_SERVICE_ROUTE))
-app.use('/api/v1/chia-service', proxy(EnvConfig.CHIA_SERVICE_ROUTE))
+// app.use('/api/v1/auth', proxy(EnvConfig.AUTH_SERVICE_ROUTE))
+// app.use('/api/v1/chia-service', proxy(EnvConfig.CHIA_SERVICE_ROUTE))
 
 app.use('/api/v1/mri-service', proxy(EnvConfig.MRI_SERVICE_ROUTE))
 app.use('/api/v1/treatment-plan-service', proxy(EnvConfig.TREATMENT_PLAN_SERVICE_ROUTE))
+app.use('/api/v1/progression-tracking-service', proxy(EnvConfig.PROGRESSION_TRACKING_SERVICE_ROUTE))
 // app.use('/api/v1/notification-service', proxy(EnvConfig.NOTIFICATION_SERVICE_ROUTE))
 // app.use('/api/v1/home-mqtt-service', proxy(EnvConfig.HOME_MQTT_SERVICE_ROUTE))
 // app.use('/api/v1/device-service', proxy(EnvConfig.DEVICE_SERVICE_ROUTE))
 // app.use('/api/v1/therapy-assistant-service', proxy(EnvConfig.START_SESSION_ROUTE))
-// app.use('/api/v1/progression-tracking-service', proxy(EnvConfig.PROGRESSION_TRACKING_SERVICE_ROUTE))
 
 //not found route
 app.use((req, res) => {
