99543a50a817ed88ec57be8be722383da294e139
Mohammad Shabeer <moshdev2213@gmail.com>
2025-04-14 11:02:38 +0530

    fix:appInFE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,6 +38,7 @@ function App() {
         <Routes>
           {/* Public Routes */}
           {/* <Route path="/" element={<h1>hello</h1>} /> */}
+
           
           {/* Auth Stack */}
           <Route path="/" element={<AuthStack />}>
